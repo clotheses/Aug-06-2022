1b16bb99366bf2406cad6a7eead9f382a55e2408
clo <clotheses@gmail.com>
2022-08-06 03:20:22 -0700

    How to use Git

---
+++ summary_using_GIT.py
@@ -0,0 +1,26 @@
+from mimetypes import init
+
+
+TOUCH file name (create a new file)
+
+ADD file name (add that file name)
+
+ADD . (add all files)
+
+then initialize the repository (on the left; source control )
+
+after that, figure how how to click COMMIT (git COMMIT) and publish it (Linux); connect to Github just by clicks
+
+note:
+
+touch
+git add
+git init
+
+
+git config --global user.name "name"
+git config --global user.email "email address"
+
+git commit
+
+and a lot of more for WINDOWS
